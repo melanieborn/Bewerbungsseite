a24c550ea1a007bc11072768b796562ba5cb1e45
melanieborn <160163854+melanieborn@users.noreply.github.com>
2024-09-23 16:01:57 +0200

    add vuetify, create Startseie vue

---
+++ myweb/src/components/StartSeite.vue
@@ -0,0 +1,38 @@
+<template>
+<v-container>
+    <v-row>
+        <v-col cols="12">
+            <p> BeWerbungsSeite </p>
+            <p> Melanie Bornkessel Junior Frontend Entwicklerin </p>
+        </v-col>
+    </v-row>
+</v-container>
+  </template>
+  
+  <script>
+  export default {
+    name: 'StartSeite',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  </style>
+  
