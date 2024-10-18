799a2d1fe0ef740f80ebeb6862c6a2aa633d9dec
melanieborn <160163854+melanieborn@users.noreply.github.com>
2024-10-18 14:47:06 +0200

    tests

--- myweb/src/main.js
+++ myweb/src/main.js
@@ -6,12 +6,13 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import router from './router'
-
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import App from './App.vue'
 
 const vuetify = createVuetify({
     components,
     directives,
+    iconfont: 'md'
   })
 
   createApp(App).use(vuetify).use(router).mount('#app')
