53d8c581459c626510d57ed057d90bf01d76e1ef
melanieborn <160163854+melanieborn@users.noreply.github.com>
2024-11-11 12:10:51 +0100

    finish respons

--- myweb/src/components/AboutMe.vue
+++ myweb/src/components/AboutMe.vue
@@ -14,31 +14,54 @@
             </v-col>
             <v-col cols="8">
               <h2 class="hidden-sm-and-down pa-5" style="font-size:2vw;">Darf ich vorstellen:</h2>
-              <h4 class="hidden-md-and-up" style="font-size:2vw;">Darf ich vorstellen:</h4>
-                <a class="links" style="font-size:1.8vw;">Ich bin Melanie Bornkessel, eine engagierte Junior Frontend Entwicklerin.
+              <h4 class="hidden-md-and-up pa-5" style="font-size:2vw;">Darf ich vorstellen:</h4>
+                <p class="links hidden-sm-and-down" >Ich bin Melanie Bornkessel, eine engagierte Junior Frontend Entwicklerin.
                   Erste Erfahrungen mit der Programmierung konnte ich während meiner Tätigkeit beim
                   Amt für kommunale Filmarbeit und dem Max-Ophüls-Festival der Landeshauptstadt Saarbrücken 
                 sammeln. Hier war ich für die Programmierung aller Kassen-, Bar- und Kinocomputer in DOS
-                zuständig.  </a> <br />
-                <a class="links" style="font-size:1.8vw;">
+                zuständig.  </p> 
+                <p class="links hidden-md-and-up resp" >Ich bin Melanie Bornkessel, eine engagierte Junior Frontend Entwicklerin.
+                  Erste Erfahrungen mit der Programmierung konnte ich während meiner Tätigkeit beim
+                  Amt für kommunale Filmarbeit und dem Max-Ophüls-Festival der Landeshauptstadt Saarbrücken 
+                sammeln. Hier war ich für die Programmierung aller Kassen-, Bar- und Kinocomputer in DOS
+                zuständig.  </p>
+                <br />
+                <p class="links hidden-sm-and-down">
                 Auch privat habe ich mich immer für Technik und Programmierung interessiert. Mit meinem
                 Umzug nach Berlin hatte ich die Möglichkeit in den technischen Bereich zu 
-                wechseln.</a>
+                wechseln.</p>
+                <p class="links hidden-md-and-up resp">
+                Auch privat habe ich mich immer für Technik und Programmierung interessiert. Mit meinem
+                Umzug nach Berlin hatte ich die Möglichkeit in den technischen Bereich zu 
+                wechseln.</p>
                 <br />
-                <a class="links" style="font-size:1.8vw;">
+                <p class="links hidden-sm-and-down">
                 Nach einem Arbeitsunfall auf Montage als Servicetechnikerin der Telekom konnte ich endlich
                 meine Passion zum Beruf machen und eine Umschulung zur Fachinformatikerin für Anwendungsentwicklung
                 erfolgreich absolvieren.
-              </a>  <br />
-              <a class="links" style="font-size:1.8vw;">
+                </p>  
+                <p class="links hidden-md-and-up resp">
+                Nach einem Arbeitsunfall auf Montage als Servicetechnikerin der Telekom konnte ich endlich
+                meine Passion zum Beruf machen und eine Umschulung zur Fachinformatikerin für Anwendungsentwicklung
+                erfolgreich absolvieren.
+                </p>
+                <br />
+              <p class="links hidden-sm-and-down">
                 Bis zu meiner Anstellung bei der FP DBS habe ich als Fullstack Entwicklerin mit PHP, HTML und CSS Erfahrungen gesammelt.
                 Bei der FP DBS habe ich als Frontend- und Backendentwicklerin gearbeitet und war in einem
                 Team zur Erstellung automatisierter Integrationtests in Python. Spezialisiert habe ich mich 
                 auf die Frontendprogrammierung, gerne nach Designvorlagen, in der ich mich weiterentwickeln möchte. 
                 Sie möchten mehr über meinen Werdegang erfahren? Kontaktieren Sie mich gerne und ich lasse Ihnen 
                 einen ausführlichen Lebenslauf zukommen.
-              </a>
-              
+              </p>
+              <p class="links hidden-md-and-up resp">
+                Bis zu meiner Anstellung bei der FP DBS habe ich als Fullstack Entwicklerin mit PHP, HTML und CSS Erfahrungen gesammelt.
+                Bei der FP DBS habe ich als Frontend- und Backendentwicklerin gearbeitet und war in einem
+                Team zur Erstellung automatisierter Integrationtests in Python. Spezialisiert habe ich mich 
+                auf die Frontendprogrammierung, gerne nach Designvorlagen, in der ich mich weiterentwickeln möchte. 
+                Sie möchten mehr über meinen Werdegang erfahren? Kontaktieren Sie mich gerne und ich lasse Ihnen 
+                einen ausführlichen Lebenslauf zukommen.
+              </p>
             </v-col>
         </v-row>
     </v-container>
